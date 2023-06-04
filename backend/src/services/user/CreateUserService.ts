5bf00c6464b1d729e62d25bd5a6bff30e15ffd94
Elton Santos <eltonaxl@hotmail.com>
2023-06-04 05:19:07 -0300

    Create user

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,6 @@
+import prismaClient from '../../prisma'
+import { hash } from 'bcryptjs'
+
 interface UserRequest{
   name: string;
   email: string;
@@ -6,10 +9,37 @@ interface UserRequest{
 
 class CreateUserService{
   async execute({ name, email, password }: UserRequest ){
-    console.log(name)
-    console.log(email)
+    
+    if(!email){
+      throw new Error("Email incorrect");
+    }
 
-    return { ok: "TESTE SUCESSO" }
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where:{
+        email: email
+      }
+    })
+
+    if(userAlreadyExists){
+      throw new Error("User/Email already exists");
+    }
+
+    const passwordHash = await hash(password, 8)
+
+    const user = await prismaClient.user.create({
+      data:{
+        name: name,
+        email: email,
+        password: passwordHash
+      },
+      select:{
+        id: true,
+        name:true,
+        email: true,
+      }
+    })
+
+    return user;
 
   }
 }
