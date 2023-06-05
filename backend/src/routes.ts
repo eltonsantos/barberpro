325f41eb0b44721e4bc93301e553fbd9d2019529
Elton Santos <eltonaxl@hotmail.com>
2023-06-05 04:10:40 -0300

    Update user

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from 'express'
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
+import { UpdateUserController } from './controllers/user/UpdateUserController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
@@ -12,5 +13,6 @@ const router = Router();
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+router.put('/users', isAuthenticated, new UpdateUserController().handle)
 
 export { router };
