0c1ad0901f45873d12126c6f7f404555daf0ff43
Elton Santos <eltonaxl@hotmail.com>
2023-06-05 03:37:06 -0300

    Create detail user

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,11 +2,13 @@ import { Router, Request, Response } from 'express'
 
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
+import { DetailUserController } from './controllers/user/DetailUserController'
 
 const router = Router();
 
 // --- ROTAS USER ---
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
+router.get('/me', new DetailUserController().handle)
 
 export { router };
