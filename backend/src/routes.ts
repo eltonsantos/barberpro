aa35a2e70275ed486e3281fcaf2b8c0ae9ae3bad
Elton Santos <eltonaxl@hotmail.com>
2023-06-05 05:13:54 -0300

    Create haircut

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,13 @@
 import { Router, Request, Response } from 'express'
 
+import { isAuthenticated } from './middlewares/isAuthenticated';
+
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
 import { UpdateUserController } from './controllers/user/UpdateUserController';
 
-import { isAuthenticated } from './middlewares/isAuthenticated';
+import { CreateHaircutController } from './controllers/haircut/CreateHaircutController';
 
 const router = Router();
 
@@ -14,5 +16,8 @@ router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
 router.put('/users', isAuthenticated, new UpdateUserController().handle)
+
+// --- ROTA HAIRCUTS ---
+router.post('/haircut', isAuthenticated, new CreateHaircutController().handle )
 
 export { router };
