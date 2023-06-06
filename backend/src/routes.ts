9c8767b853aa63c59406efa6295fbdab1b9b9ba8
Elton Santos <eltonaxl@hotmail.com>
2023-06-06 04:34:52 -0300

    Finish schedule

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -16,6 +16,7 @@ import { DetailHaircutController } from './controllers/haircut/DetailHaircutCont
 
 import { NewScheduleController } from './controllers/schedule/NewScheduleController'
 import { ListScheduleController } from './controllers/schedule/ListScheduleController'
+import { FinishScheduleController } from './controllers/schedule/FinishScheduleController'
 
 const router = Router();
 
@@ -36,5 +37,6 @@ router.get('/haircut/detail', isAuthenticated, new DetailHaircutController().han
 // --- ROTA SCHEDULE / SERVIÇOS ---
 router.post('/schedule', isAuthenticated, new NewScheduleController().handle)
 router.get('/schedules', isAuthenticated, new ListScheduleController().handle)
+router.delete('/schedule', isAuthenticated, new FinishScheduleController().handle)
 
 export { router };
