896418e19f044a7b302e5d4b1627c29b2df96703
Elton Santos <eltonaxl@hotmail.com>
2023-06-06 04:13:42 -0300

    Create schedule

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -14,6 +14,8 @@ import { CheckSubscriptionController } from './controllers/haircut/CheckSubscrip
 import { CountHaircutsController } from './controllers/haircut/CountHaircutsController'
 import { DetailHaircutController } from './controllers/haircut/DetailHaircutController'
 
+import { NewScheduleController } from './controllers/schedule/NewScheduleController'
+
 const router = Router();
 
 // --- ROTAS USER ---
@@ -29,5 +31,8 @@ router.put('/haircut', isAuthenticated, new UpdateHaircutController().handle)
 router.get('/haircut/check', isAuthenticated, new CheckSubscriptionController().handle)
 router.get('/haircut/count', isAuthenticated, new CountHaircutsController().handle)
 router.get('/haircut/detail', isAuthenticated, new DetailHaircutController().handle)
+
+// --- ROTA SCHEDULE / SERVIÇOS ---
+router.post('/schedule', isAuthenticated, new NewScheduleController().handle)
 
 export { router };
