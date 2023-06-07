88d3bc1f1fcd98886b1a03202bfbb64332585394
Elton Santos <eltonaxl@hotmail.com>
2023-06-07 01:39:11 -0300

    Initial web project

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home(){
+  return(
+    <>
+      <Head>
+        <title>BarberPRO - Seu sistema completo</title>
+      </Head>
+      <div>
+        <h1>BarberPRO</h1>
+      </div>
+    </>
+  )
+}
