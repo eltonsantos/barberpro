cff804bcbac1789e2abd79329dac7eaab131c547
Elton Santos <eltonaxl@hotmail.com>
2023-06-08 01:08:53 -0300

    Create login and register page

--- web/src/pages/register/index.tsx
+++ web/src/pages/register/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Head from 'next/head'
 import Image from 'next/image';
 import logoImg from '../../../public/images/logo.svg'
@@ -5,11 +6,22 @@ import { Flex, Text, Center, Input, Button } from '@chakra-ui/react'
 
 import Link from 'next/link'
 
-export default function Login(){
+export default function Register(){
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleRegister(){
+    console.log(name)
+    console.log(email)
+    console.log(password)
+  }
+
+
   return(
     <>
       <Head>
-        <title>BarberPRO - Faça login para acessar</title>
+        <title>Cria sua conta no BarberPRO</title>
       </Head>
       <Flex background="barber.900" height="100vh" alignItems="center" justifyContent="center">
         
@@ -28,9 +40,22 @@ export default function Login(){
             background="barber.400"
             variant="filled"
             size="lg"
+            placeholder="Nome da barbearia"
+            type="text"
+            mb={3}
+            value={name}
+            onChange={ (e) => setName(e.target.value) }
+          />
+
+          <Input
+            background="barber.400"
+            variant="filled"
+            size="lg"
             placeholder="email@email.com"
             type="email"
             mb={3}
+            value={email}
+            onChange={ (e) => setEmail(e.target.value) }
           />
 
           <Input
@@ -40,21 +65,25 @@ export default function Login(){
             placeholder="********"
             type="text"
             mb={6}
+            value={password}
+            onChange={ (e) => setPassword(e.target.value) }
           />
 
           <Button
+            onClick={handleRegister}
             background="button.cta"
             mb={6}
             color="gray.900"
             size="lg"
             _hover={{ bg: "#ffb13e" }}
           >
-            Acessar
+            Cadastrar
           </Button>
 
+
           <Center mt={2}>
-            <Link href="/register">
-              <Text cursor="pointer" color="#ddd">Ainda não possui conta? <strong>Cadastre-se</strong></Text>
+            <Link href="/login">
+              <Text cursor="pointer" color="#ddd">Já possui uma conta? <strong>Faça login</strong></Text>
             </Link>
           </Center>
 
