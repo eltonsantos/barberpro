c0bf5ae807d8d585b148a357084c6783a7b51071
Elton Santos <eltonaxl@hotmail.com>
2023-06-08 18:15:20 -0300

    Protect routes

--- web/src/pages/dashboard/index.tsx
+++ web/src/pages/dashboard/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import { Flex, Text } from '@chakra-ui/react';
 
+import { canSSRAuth } from '../../utils/canSSRAuth'
+
 export default function Dashboard(){
   return(
     <>
@@ -13,3 +15,12 @@ export default function Dashboard(){
     </>
   )
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+
+  return {
+    props: {
+      
+    }
+  }
+})
