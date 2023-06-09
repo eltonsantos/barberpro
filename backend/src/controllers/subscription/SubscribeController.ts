b3e61b11b5a53f9f5f9b630c30cda9b8a8ca6202
Elton Santos <eltonaxl@hotmail.com>
2023-06-09 04:26:18 -0300

    Routes to stripe

--- backend/src/controllers/subscription/SubscribeController.ts
+++ backend/src/controllers/subscription/SubscribeController.ts
@@ -1,11 +1,11 @@
-import { Response, Request } from "express";
+import {Request, Response } from 'express'
 import { SubscribeService } from '../../services/subscription/SubscribeService'
 
 class SubscribeController{
   async handle(request: Request, response: Response){
-    const user_id = request.user_id;
+    const user_id = request.user_id
 
-    const subscribeService = new SubscribeService();
+    const subscribeService = new SubscribeService()
 
     const subscribe = await subscribeService.execute({
       user_id
